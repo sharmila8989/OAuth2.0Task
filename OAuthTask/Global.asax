95e13b749926c7e15658500f66fdd43d65fb0f68
sharmila8989 <sharmila8989@users.noreply.github.com>
2018-10-12 17:53:54 +1100

    final commit

---
+++ OAuthTask/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OAuthTask.MvcApplication" Language="C#" %>
---
+++ OAuthTask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAuthTask.Startup))]
+namespace OAuthTask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
